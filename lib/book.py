05910eee6d70a0afd4e8dc62a25360370397a71a
nungarimuchiru-hue <nungari.muchiru@student.moringaschool.com>
2026-06-12 15:34:11 +0300

    Complete OOP bookstore

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self,title,author,total_pages):
+        self.title = title
+        self._author = author
+        self._total_pages = total_pages
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def total_pages(self):
+        return self._total_pages    
