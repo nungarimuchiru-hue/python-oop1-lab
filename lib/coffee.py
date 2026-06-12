1c0c082a932cdf209f09f4a005a08ee3c7335d44
nungarimuchiru-hue <nungari.muchiru@student.moringaschool.com>
2026-06-12 16:28:36 +0300

    Add setter and validation for size property

--- lib/coffee.py
+++ lib/coffee.py
@@ -2,11 +2,7 @@
 
 class Coffee:
     def __init__(self, size: str, price: float):
-        valid_sizes = ["small", "medium", "large"]
-        if not isinstance(size,str) or size.lower() not in valid_sizes:
-            raise ValueError("size must be Small, Medium, or Large")
-        
-        self.size = size.capitalize()
+        self.size = size
         self._base_price = price
         self._tip_amount = 0.0
         
@@ -14,11 +10,18 @@ class Coffee:
     def size(self):
         return self._size
     
+    @size.setter
+    def size(self, value: str):
+        valid_sizes = ["small", "medium", "large"]
+        if not isinstance(value, str) or value.lower() not in valid_sizes:
+            raise ValueError("size must be Small, Medium, or Large")
+        self._size = value.capitalize()
+
     @property
     def price(self):
         return self._base_price + self._tip_amount
-    
+
     def tip(self, amount: float = None):
         if amount is not None:
             self._tip_amount += amount
-            return self._tip_amount
+        return self._tip_amount
