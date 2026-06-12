f9d3d701dee33e4b19d67c3814ba9cf4b276aff1
nungarimuchiru-hue <nungari.muchiru@student.moringaschool.com>
2026-06-12 15:56:40 +0300

    Fix Coffee class constructor and attributes

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size: str, price: float, tip:float = 0.0):
+        valid_sizes = ['small', 'medium', 'large']
+        if size.lower() not in valid_sizes:
+            raise ValueError("Invalid size")
+        
+        self.size = size
+        self._base_price = price
+        self.tip = tip
+        
+
+        @property
+        def name(self):
+            return self._base_price + self.tip
+        
+        def add_tip(self,amount: float):
+            self.tip += amount
